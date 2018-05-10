304ca9375aa5fc9b776ce5012be58b77faeac7d8
SCP-028 <SCP-028@users.noreply.github.com>
2018-05-10 19:58:28 -0400

    Test vgo for package control.

---
+++ main.go
@@ -0,0 +1,30 @@
+package main // import "github.com/SCP-028/vuelog"
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	// in case Nginx stops working (very unlikely)
+	r.Use(static.Serve("/static", static.LocalFile("./dist/static", true)))
+	r.LoadHTMLGlob("./dist/index.html")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	// Let's Encrypt
+	r.StaticFS("/.well-known", http.Dir("./.well-known"))
+	// group: api
+	api := r.Group("/api")
+	{
+		api.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+	r.Run(":9587")
+}
