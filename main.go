17104566eae8dec77facf35d910f694649c04d9f
SCP-028 <SCP-028@users.noreply.github.com>
2018-06-30 16:03:59 +0800

    Test connection to database.

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main // import "github.com/SCP-028/vuelog"
 import (
 	"net/http"
 
+	"database/db"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	db.Init()
 	// group: api
 	api := r.Group("/api")
 	{
